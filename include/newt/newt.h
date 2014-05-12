b34624a9fa5d6f7c0907b4f2d56b565e54ac93d6
bmoir <bmoir@ualberta.net>
2014-05-12 10:18:51 -0700

    first commit

---
+++ include/newt/newt.h
@@ -0,0 +1,12 @@
+/*******************************************************************************
+*	This file is small, but it is here to follow the format of the other   *
+*		programs, it determines the intensity of a point as seen by    *
+*		an observer.						       *
+*******************************************************************************/
+
+double I_em(double alpha){
+double I;
+I=I_o*(1.0-cos(alpha));
+return I;
+}
+
